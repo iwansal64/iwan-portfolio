86a8f1663b2e252bc4f3c020bb9046f57fc73cf4
Iwansal64 <iwansal64@gmail.com>
2025-06-22 16:57:05 +0700

    [feat](sidebar) Create navbar for mobile view: sidebar :D

---
+++ src/components/Navbar/Sidebar.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return <>
+  <button className="lg:hidden fixed top-4 left-4 text-3xl cursor-pointer z-101 w-[50px] h-[50px]" onClick={() => setIsOpen(!isOpen)}>
+    <span className={`absolute left-1/2 -translate-x-1/2 top-1/2 h-1 w-full bg-white duration-500 ${isOpen ? "-translate-y-1/2 -rotate-225" : "-translate-y-[calc(50%_-_8px)]"}`}></span>
+    <span className={`absolute left-1/2 -translate-x-1/2 top-1/2 h-1 w-full bg-white duration-500 ${isOpen ? "-translate-y-1/2 -rotate-135" : "-translate-y-[calc(50%_+_8px)]"}`}></span>
+  </button>
+  <div className={`lg:hidden w-full h-full flex flex-col justify-between fixed z-100 px-4 pb-8 pt-24 duration-1000 ${isOpen ? "bg-black" : "bg-transparent pointer-events-none"}`}>
+    <div className={`flex flex-col text-4xl delay-500 duration-500 ${isOpen ? "" : "delay-0! -translate-x-[100%]"}`}>
+      <a href="#">1. INTRO</a>
+      <a href="#about">2. ABOUT</a>
+      <a href="#project">3. PROJECTS</a>
+      <a href="#contact">4. CONTACT</a>
+    </div>
+    <div>
+      <p className={`text-sm delay-500 duration-500 ${isOpen ? "" : "delay-0! -translate-x-[100%]"}`}>Developed by <span className="italic font-bold">Ridwan Bagoes Setiawan</span> with 💖</p>
+    </div>
+  </div>
+  </>;
+}
